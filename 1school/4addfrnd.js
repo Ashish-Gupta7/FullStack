679acb08926d13e6aec626f4023e3aad9a826fd7
Ashish-Gupta7 <ash300mr@gmail.com>
2023-11-24 23:56:16 +0530

    rohitsharma

--- 1school/4addfrnd.js
+++ 1school/4addfrnd.js
@@ -8,16 +8,21 @@ btn.addEventListener("click", function(){
     frnd.innerHTML = "sent...";
     frnd.style.color = "white";
     btn.innerHTML = "cancel";
-    setTimeout( function(){
+    setTimeout(function(){
         frnd.innerHTML = "Friend 💖";
         frnd.style.color = "rgb(100, 255, 100)";
         btn.innerHTML = "Cancel";
         check = 1;
-    }, 3000)}
+    }, 3500)}
     else{
-        frnd.innerHTML = "Know Everyone";
-        frnd.style.color = "rgb(255, 194, 103)";
-        btn.innerHTML = "Add Friend";
+        frnd.innerHTML = "Make a mistake";
+        frnd.style.color = "red";
+        btn.innerHTML = "Removing...";
         check = 0;
+        setTimeout(function(){
+            frnd.innerHTML = "Know Everyone";
+            frnd.style.color = "rgb(255, 194, 103)";
+            btn.innerHTML = "Add Friend";
+        }, 2000)
     }
 })
