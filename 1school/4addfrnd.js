3a444f06bc147fa43d525b9dad861221f688d3b5
Ashish-Gupta7 <ash300mr@gmail.com>
2023-11-25 14:01:16 +0530

    rs

--- 1school/4addfrnd.js
+++ 1school/4addfrnd.js
@@ -1,28 +1,31 @@
-var btn = document.querySelector("button");
-var frnd = document.querySelector(".frnd");
+var btn1 = document.querySelector(".button1");
+var frnd1 = document.querySelector(".frnd1");
 
 var check = 0;
 
-btn.addEventListener("click", function(){
-    if(check == 0){
-    frnd.innerHTML = "sent...";
-    frnd.style.color = "white";
-    btn.innerHTML = "cancel";
-    setTimeout(function(){
-        frnd.innerHTML = "Friend 💖";
-        frnd.style.color = "rgb(100, 255, 100)";
-        btn.innerHTML = "Cancel";
-        check = 1;
-    }, 3500)}
-    else{
-        frnd.innerHTML = "Make a mistake";
-        frnd.style.color = "red";
-        btn.innerHTML = "Removing...";
+btn1.addEventListener("click", function () {
+    if (check == 0) {
+        frnd1.innerHTML = "sent...";
+        frnd1.style.color = "white";
+        btn1.innerHTML = "cancel";
+        setTimeout(function () {
+            frnd1.innerHTML = "Friend 💖";
+            frnd1.style.color = "rgb(100, 255, 100)";
+            btn1.innerHTML = "Cancel";
+            check = 1;
+        }, 3500)
+    }
+    else {
+        frnd1.innerHTML = "Make a mistake";
+        frnd1.style.color = "red";
+        btn1.innerHTML = "Removing...";
         check = 0;
-        setTimeout(function(){
-            frnd.innerHTML = "Know Everyone";
-            frnd.style.color = "rgb(255, 194, 103)";
-            btn.innerHTML = "Add Friend";
+        setTimeout(function () {
+            frnd1.innerHTML = "Know Everyone";
+            frnd1.style.color = "rgb(255, 194, 103)";
+            btn1.innerHTML = "Add Friend";
         }, 2000)
     }
 })
+
+
