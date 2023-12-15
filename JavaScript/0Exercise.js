6d050859544c9d892309bded288b8773bea6a883
Ashish-Gupta7 <ash300mr@gmail.com>
2023-12-15 21:15:15 +0530

    15Dec

---
+++ JavaScript/0Exercise.js
@@ -0,0 +1,8 @@
+// Exercise1 =>
+
+// let p = document.querySelector("p");
+// let attr = p.getAttribute("class");
+// // console.log(attr);
+// // p.setAttribute("class", "newPara");    // setAttribute use karne ki wajah se yaha class overwrite kr jati h mtlb class para ht jati h jb class newPara ko use krte h to, lekin que. ke anusar hm chahte h ki dono classes execute ho koi bhi ek doosre ko overwrite n kare. iske liye hi classList ko use karege.
+
+// p.classList.add("newPara");    // p tag me ek aur class newPara ko add kiya gya h.
