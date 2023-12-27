57940aede2ea5e26a3dcd3f6dfabcc23f72693f9
Ashish-Gupta7 <ash300mr@gmail.com>
2023-12-27 13:34:42 +0530

    quesolving

---
+++ 1school/8QueSolving.js
@@ -0,0 +1,67 @@
+// Que.1 => Make a variable user and initialize a value "Ram" and print it on console.
+
+// var a;
+// a = "Ram";
+// console.log(a);
+
+// Que.2 => Make two variables and print their sum, mul, div, sub.
+
+// var a, b;
+// a = 10;
+// b = 5;
+// console.log("sum =", a+b);
+// console.log("sum =", a-b);
+// console.log("sum =", a*b);
+// console.log("sum =", a/b);
+
+// Que.3 => Write a JavaScript program to find the area of a triangle where three sides are 5, 6, 7. 
+
+// Area = root{s.(s-a).(s-b).(s-c)}
+// s = ( a + b + c ) / 2
+// var a, b, c, s, A, Area;
+// a = 5; b = 6; c = 7;
+
+// s = ( a + b + c ) / 2;
+// Area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+// console.log(Area);
+
+// Que.4 => Write a JavaScript program to compute the sum of the two given integers. If the two values are the same, then print triple their sum.
+
+// var a, b;
+// a = Number(prompt("Enter a number"));
+// b = Number(prompt("Again, Enter a number"));
+// if(a === b){
+//     console.log(3 * (a + b));
+// } else {
+//     console.log(a + b);
+// }
+
+// Que.5 => Accept a number from a user and print it on console.
+
+// var a;
+// a = Number(prompt("Enter a number"));
+// console.log(a);
+
+// Que.6 => Ask a user his/her name and say hello (eg. "Hello Sarthak").
+
+// var Name;
+// Name = prompt("Enter your name");
+// console.log("Hello", Name);
+
+// Que.7 => Accept two integers from user and print the sum
+//    Ex - The sum of 45 & 12 = 57
+
+// var a, b;
+// a = Number(prompt("Enter a number"));
+// b = Number(prompt("Again, Enter a number"));
+// console.log(`The sum of ${a} & ${b} = ${a+b}`);
+
+// Que.8 => Accept the User's name, age and print in following manner
+//    Ex - Hello Shery, you are 12 years old.
+
+// var a, b;
+// a = prompt("Enter your name");
+// b = prompt("Enter your age");
+// console.log(`Hello ${a}, you are ${b} years old.`);
+
+// Que.9 => Accept two numbers and print the greatest between them.
