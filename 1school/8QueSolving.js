bea6138ec849a556ad51e209ed37d31b199fe76c
Ashish-Gupta7 <ash300mr@gmail.com>
2024-01-12 15:26:54 +0530

    js que

--- 1school/8QueSolving.js
+++ 1school/8QueSolving.js
@@ -1,8 +1,24 @@
+// // (0.) Hello world program =>
+// // 1st method =
+// var a = "Hello World";
+// console.log(a);
+// // 2nd method =
+// var body = document.querySelector("body");
+// var b = document.createElement("h1");
+// var c = b.innerText = "Hello World";
+// body.prepend(b);
+// // 3rd & 4th method =
+// alert("Hello World");
+// prompt("say hello world");
+// // 5th method =
+// document.write("Hello World");
+
 // Que.1 => Make a variable user and initialize a value "Ram" and print it on console.
 
 // var a;
 // a = "Ram";
 // console.log(a);
+
 
 // Que.2 => Make two variables and print their sum, mul, div, sub.
 
@@ -327,6 +343,24 @@
 // 		console.log("Invalid input, Please Enter valid number.");
 // }
 
+// Using Recursion => The process in which a function calls itself directly or indirectly is called recursion and the corresponding function is called as recursive function.
+// var number = prompt("Enter a number: ");
+// var fact = 1;
+// if(number !== null && number !== ""){    // ye check karega ki user ne koi number enter kiya ya nhi.
+//     function factorial(n){
+//         if(n > number){
+//             return;    // ye tb tk run nhi hoga jb tk ki n ki value user dwara diye gaye number se bada n ho jae.
+//         }
+//         fact = fact * n;
+//         console.log(`${n} x ${fact} = ${fact}`);
+//         n++;
+//         factorial(n);  // ye n++ ko uper function me lekr jayega.
+//     }
+//     factorial(1);
+// } else{
+//     console.log("Please, Enter a number: ");
+// }
+
 // Que.28 => Print the sum of all even & odd numbers in a range seperately.
 
 // var n = parseInt(prompt("Enter a number:"));
@@ -462,3 +496,282 @@
 // 	}
 // }
 // checkPrimeNumber();
+
+// Que.36 => Swap two Numbers Using temporary Variable & Without Temporary Variable.
+// var a = prompt("Enter a number");
+// var b = prompt("Enter a number");
+// console.log(`The value of a is: ${a} and The value of b is: ${b}`);
+// var temp;
+// temp = a;
+// a = b;
+// b = temp;
+// console.log(`Swapped Value => The value of a is: ${a} and The value of b is: ${b}`);
+
+// Que.37 => Convert Kilometers to Miles.
+// var a = prompt("Enter kilometer");
+// var b = a * 0.621371;
+// console.log(`You entered in kilometers: ${a} \nConverted into Miles: ${b}`);
+
+// Que.38 => Convert Celsius to Fahrenheit using.
+// var celsius = prompt("Enter celsius value");
+// var fahrenheit = (9/5 * celsius) + 32;
+// console.log(`You entered in celsius: ${celsius} Degree \nConverted into Fahrenheit: ${fahrenheit} Degree`);
+
+// Que.39 => Check if a number is Positive, Negative, Decimal or Zero.
+// var number = Number(prompt("Enter any number: "));
+// if(number === 0){
+//     console.log(`You entered: ${number}, this is Zero.`);
+// } else if(number > 0 && number !== Math.floor(number)){
+//     console.log(`You entered: ${number}, this is a Positive Decimal Number.`);
+// } else if(number < 0 && number !== Math.floor(number)){
+//     console.log(`You entered: ${number}, this is a Negative Decimal Number.`);
+// } else if(number < 0){
+//     console.log(`You entered: ${number}, this is a Negative Number.`);
+// } else if(number > 0){
+//     console.log(`You entered: ${number}, this is a Positive Number.`);
+// } else{
+//     console.log("Please, Enter any number: ");
+// }
+
+// Que.40 => Program to print the Table of any user defined number using function.
+// var number = parseInt(prompt("Enter a number"));
+// var table = "";
+// for(i = 1; i <= 10; i++){
+//     table = number * i;
+//     console.log(`${number} x ${i} = ${table}`);
+// }
+
+// Using function =>
+// function table(number){
+//     var multiply = "";
+//     for(i = 1; i <= 10; i++){
+//         multiply = number * i;
+//         console.log(`${number} x ${i} = ${multiply}`);
+//     }
+// }
+// table(4);
+// table(5);
+
+// Que.41 => Program to make a simple calculator using switch case.
+// Using if-else =>
+// var number1 = prompt("Enter a number1:");
+// var number2 = prompt("Enter a number2:");
+// var calculate = prompt("What you want to calculate => +, -, x, /");
+// if(calculate === "+" || calculate === "plus" || calculate === "Plus"){
+//     Addition = number1 + number2;
+//     console.log(`${number1} + ${number2} = ${Addition}`);
+// } else if(calculate === "x" || calculate === "multiply" || calculate === "Multiply" || calculate === "*"){
+//     Multiplication = number1 * number2;
+//     console.log(`${number1} x ${number2} = ${Multiplication}`);
+// } else if(calculate === "-" || calculate === "minus" || calculate === "Minus"){
+//     Substraction = number1 - number2;
+//     console.log(`${number1} - ${number2} = ${Substraction}`);
+// } else if(calculate === "/" || calculate === "divide" || calculate === "Divide"){
+//     Division = number1 / number2;
+//     console.log(`${number1} / ${number2} = ${Division}`);
+// } else{
+//     console.log("Please, Enter a number");
+// }
+
+// Using switch case =>
+// var number1 = Number(prompt("Enter a number1"));
+// var number2 = Number(prompt("Enter a number2"));
+// var calculate = prompt("What you want to calculate => +, -, x, /");
+// switch (calculate){
+//     case "+":
+//         console.log(`${number1} + ${number2} =`, number1 + number2);
+//         break;
+
+//     case "-":
+//         console.log(`${number1} - ${number2} =`, number1 - number2);
+//         break;
+
+//     case "x":
+//         console.log(`${number1} + ${number2} =`, number1 * number2);
+//         break;
+
+//     case "/":
+//         console.log(`${number1} + ${number2} =`, number1 / number2);
+//         break;
+
+//     default:
+//         console.log("Please, Enter a number");
+//         break;
+// }
+
+// Que.42 => Program to check if the numbers have same last digit.
+// var num1 = Number(prompt("Enter 1st number: "))
+// var num2 = Number(prompt("Enter 2nd number: "))
+// var num3 = Number(prompt("Enter 3rd number: "))
+// var Rem1 = num1 % 10;
+// var Rem2 = num2 % 10;
+// var Rem3 = num3 % 10;
+// console.log(`${Rem1} ${Rem2} ${Rem3}`);
+// if(Rem1 === Rem2 && Rem2 === Rem3 && Rem3 === Rem1){
+//     console.log("Last digit of all three numbers are equal.");
+// } else if(Rem1 === Rem2){
+//     console.log("Last digit of num1 and num2 are equal.");
+// } else if(Rem2 === Rem3){
+//     console.log("Last digit of num2 and num3 are equal.");
+// } else if(Rem1 !== Rem2 !== Rem3){
+//     console.log("Last digit of all three numbers are not equal.");
+// } else{
+//     console.log("Please, Enter number:");
+// }
+
+// Que.43 => Program to print the Fibonacci sequence.
+// फिबोनाची श्रृंखला => शून्य से शुरू होने वाली संख्याओं का एक क्रम है, जो इस तरह से व्यवस्थित हैं कि श्रृंखला में किसी भी संख्या का मूल्य पिछले दो संख्याओं का जोड़ है। 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610… मतलब पिछली दो संख्याओं के जोड़ से अगली संख्या बनती है और ये ऐसे ही श्रृंखला अनंत तक चलती है।
+// var a = 0, b = 1;
+// console.log(a);
+// console.log(b);
+// for(var i = 0; i <= 45; i++){    // ye 45 tk ki series h.
+//     var temporary = a + b;
+//     console.log(temporary);      // a, b, temporary = 0, 1, 1
+//     a = b;                       // b ki value ko a ki value bana diya.
+//     b = temporary;               // temporary ki value ko b ki value bana diya.
+// }                                // ab wapas se loop chalega aur a aur b ko add krke temporary me save kr dega.
+
+// Que.44 => Program to check Armstrong Number.
+// Armstrong number => In the case of an Armstrong number of 3 digits, the sum of cubes of each digit is equal to the number itself. 
+// For example, 153 = 1*1*1 + 5*5*5 + 3*3*3
+// Armstrong Numbers =>
+// 1 digit -> 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+// 2 digit -> No Armstrong Numbers 
+// 3 digit -> 153, 370, 371, 407
+// 4 digit -> 1634, 8208, 9474 
+// chatGPT and me =>
+// var myNumber = prompt("Enter a number: ");
+// var digits = myNumber.toString().length;
+// console.log(digits);
+// function digitsToArray(number){
+//     // Convert the number to a string
+//     const numberString = number.toString();
+//     // Split the string into an array of characters
+//     const digitsArray = numberString.split('').map(Number);
+//     return digitsArray;
+// }
+// const resultArray = digitsToArray(myNumber);
+// console.log(resultArray);
+
+// // Using map to raise each element to the power
+// var poweredArray = resultArray.map(function(n) {
+//     return Math.pow(n, digits);
+// });
+
+// // Using reduce to calculate the sum
+// var sum = poweredArray.reduce(function(acc, value) {
+//     return acc + value;
+// }, 0);
+
+// // Output the sum
+// console.log(sum);
+// if(sum == myNumber){
+//     console.log(`${myNumber} is an Armstrong Number.`);
+// } else{
+//     console.log(`${myNumber} is not an Armstrong Number.`);
+// }
+
+// 2nd method YT =>
+// var number = Number(prompt("Enter a number: "));
+// var temporary = number;
+// var digits = Number(number.toString().length);
+// console.log(`Digits = ${digits}`);
+// var sum = 0;
+// while(temporary > 0){
+//     var num = temporary % 10;
+//     console.log(`module = ${num}`);
+//     var sum = sum + num ** digits;
+//     console.log(`sum = ${sum}`);
+//     var leftNumber = temporary / 10;
+//     console.log(`divided number = ${leftNumber}`);
+//     var convertInt = parseInt(leftNumber);
+//     console.log(`divided number convert into integer = ${convertInt}`);
+//     temporary = convertInt;
+//     console.log(`----------------------\n \n`);
+// }
+// if(sum == number){
+//     console.log("Armstrong number");
+// } else{
+//     console.log("not an Armstrong number");
+// }
+
+// 3rd method only chapGPT =>
+// function isArmstrongNumber(number) {
+//     // Convert the number to a string to determine the number of digits
+//     const numString = number.toString();
+//     const numDigits = numString.length;
+
+//     // Calculate the sum of digits each raised to the power of the number of digits
+//     let sum = 0;
+//     for (let i = 0; i < numDigits; i++) {
+//         sum += Math.pow(parseInt(numString[i]), numDigits);
+//     }
+
+//     // Check if the sum is equal to the original number
+//     return sum === number;
+// }
+
+// // Example usage
+// const testNumber = 153; // Change this to test different numbers
+// if (isArmstrongNumber(testNumber)) {
+//     console.log(`${testNumber} is an Armstrong number.`);
+// } else {
+//     console.log(`${testNumber} is not an Armstrong number.`);
+// }
+
+// Que.45 => Program to find Armstrong number in an Interval.
+// mtlb user se hm do number mangte h aur usi ke beech me check karenge ki kitne Armstrong number h.
+// var num1 = Number(prompt("Please, Enter lower number; "))
+// var num2 = Number(prompt("Please, Enter upper number; "))
+// console.log(`Armstrong Numbers, Range of ${num1} to ${num2} /-`);
+// for (var i = num1; i <= num2; i++) {
+//     var temporary = i;
+//     var digits = Number(i.toString().length);
+//     var sum = 0;
+//     while (temporary > 0) {
+//         var num = temporary % 10;
+//         var sum = sum + num ** digits;
+//         var leftNumber = temporary / 10;
+//         var convertInt = parseInt(leftNumber);
+//         temporary = convertInt;
+//     }
+//     if(sum == i){
+//         console.log(i);
+//     }
+// }
+
+// Que.46 => Program to check Palindrome using for loop.
+// Palindrome string => A string is a palindrom if it is read the same from forward or backward.
+// Example => dad, madam
+// var string = prompt("Please, Enter a string:")
+// var len = string.length;
+// var msg = `${string}, It is a Palindrome string.`;
+// // string ki counting index ke roop me hoti h isiliye i = 0 h. ex ke liye user dad enter krta h to console pr i ki value 0 aur 1 print hogi. 0 to phle hi chal jayega aur dad me 3 letters h ise 2 se divide krne pr 1 aayega to loop ek baar aur chalega.
+// for(var i = 0; i < len/2; i++){      
+//     // same ex ke anusar string dad ki 0th index "d" aur 1st index "a" print hogi.
+//     console.log(string[i]);   
+//     // same ex /- len = 3 h, 3-1-i(i = 0) = 2 mtlb dad me 2nd index pr "d" h, aur isi prakar 3-1-i(i = 1) = 1 yaha 1st index pr "a" h.
+//     console.log(string[len - 1 - i]);
+//     if(string[i] != string[len - 1 - i]){
+//         msg = `${string}, It is not a Palindrome string.`;
+//     }
+// }
+// console.log(msg);
+
+// Que.47 => Program to sort words in Alphabetical Order.
+// var string = prompt("Enter a striing: ");
+// var stringArray = string.split(" ");    // string ko words ke roop me Array me convert kr dega.
+// stringArray.sort();    // ye array hi return krta h aur ye phle capital aur phir small letter ko arrange krta h.
+// console.log(string);
+// console.log(stringArray);
+// console.log(stringArray.join(" "));    // ye array ko wapas string me convert krta h.
+
+// Que.48 => Program to replace characters of a string.
+// var string = "Mr. Red has a red car and red bike."
+// console.log(string);
+// var reg = RegExp("red", "gi");
+// var newstr = string.replace(reg, "blue");
+// console.log(newstr);
+
+// Que.49 => Program to reverse a string using for loop.
+var string = prompt("Enter a string: ")
