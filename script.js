fbbe07dba1368f303f83e9be66dfd43badfb03fb
Ashish-Gupta7 <ash300mr@gmail.com>
2024-01-23 17:38:15 +0530

    these files created for practice

---
+++ script.js
@@ -0,0 +1,37 @@
+// (1.) area of a rectangle.
+// var a = Number(prompt("Enter height"));
+// var b = Number(prompt("Enter width"));
+// var c = a*b;
+// console.log(c);
+
+// (2.) ask user's electricity unit. if unit is 100 or above then 
+// var a = Number(prompt("Enter your electricity units: "));
+// if( a >= 100 ){
+//     console.log(`your electricity bill is ${a * 10} rs.`);
+// } else if(a > 70 && a < 100){
+//     console.log(`your electricity bill is ${a * 8} rs.`);
+// } else if(a <= 70){
+//     console.log(`your electricity bill is ${a * 5} rs.`);
+// }
+
+// (3.) 
+// var a = Number(prompt("Enter any number:"));
+// if(a%5 == 0){
+//     console.log("jhanddu baam");
+// }
+
+// var a = Number(prompt("Enter any number:"));
+// for(var i = 1; i < a; i++){
+//     if(i%5 == 0){
+//         console.log("Jhandddddduuuuuu Baaaaam");
+//     }else{
+//         console.log(i);
+//     }
+// }
+
+// (4.)
+// function greet(user, age){
+//     var remain = 60 - age
+//     console.log(`Hello, ${user} You will live for ${remain} more years. `);
+// }
+// greet("qwer", 20);
