669a04c22eb2035e94b5da615278bb064d604333
Ashish-Gupta7 <ash300mr@gmail.com>
2024-02-27 09:45:15 +0530

    some changes

--- 10textRelatedAnimation/1textHiddenSplit.js
+++ 10textRelatedAnimation/1textHiddenSplit.js
@@ -13,41 +13,55 @@ function splitting1() {
 splitting1();
 
 var main1 = document.querySelector("#main1");
+var isAnimating = false;
 main1.addEventListener("mouseenter", function () {
-    var tl1 = gsap.timeline();
-    tl1.to("#main1 h1 span", {
-        opacity: 0,
-        duration: .5,
-        stagger: {
-            amount: 1
-        },
-    }, "same")
-    tl1.to("#main1 h2 span", {
-        opacity: 1,
-        delay: .3,
-        duration: .5,
-        stagger: {
-            amount: 1
-        },
-    }, "same")
+    if (!isAnimating) {
+        isAnimating = true;
+        var tl1 = gsap.timeline();
+        tl1.to("#main1 h1 span", {
+            opacity: 0,
+            duration: .5,
+            stagger: {
+                amount: 1
+            },
+        }, "same")
+        tl1.to("#main1 h2 span", {
+            opacity: 1,
+            delay: .3,
+            duration: .5,
+            stagger: {
+                amount: 1
+            },
+            onComplete: function () {
+                isAnimating = false;
+            }
+        }, "same")
+    }
 })
-main1.addEventListener("mouseleave", function () {
-    var tl1 = gsap.timeline();
-    tl1.to("#main1 h2 span", {
-        opacity: 0,
-        duration: .5,
-        stagger: {
-            amount: 1
-        },
-    }, "any")
-    tl1.to("#main1 h1 span", {
-        opacity: 1,
-        delay: .3,
-        duration: .5,
-        stagger: {
-            amount: 1
-        },
-    }, "any")
 
-})  
+main1.addEventListener("mouseleave", function () {
+    if (!isAnimating) {
+        isAnimating = true;
+        var tl1 = gsap.timeline();
+        tl1.to("#main1 h2 span", {
+            opacity: 0,
+            duration: .5,
+            stagger: {
+                amount: 1
+            },
+        }, "any")
+        tl1.to("#main1 h1 span", {
+            opacity: 1,
+            delay: .3,
+            duration: .5,
+            stagger: {
+                amount: 1
+            },
+            onComplete: function () {
+                isAnimating = false;
+            }
+        }, "any")
+
+    }
+})
 // Gsap me kisi bhi do elements ko same variable Dene pr vo elements ek hi time pr work krte h. Ye variable un dono ke last me curly bracket ke phle double quotes me likhte h. jaisa ki yaha "same" ko likha gya h.
