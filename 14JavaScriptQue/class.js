e737ac798a6bb03020e14686ddd9bfd2e13ef9ed
Ashish-Gupta7 <ash300mr@gmail.com>
2024-03-11 14:15:04 +0530

    filterProduct add new products

--- 14JavaScriptQue/class.js
+++ 14JavaScriptQue/class.js
@@ -68,7 +68,7 @@
 // }
 // var myArray = [{a: 1, b: 2}, {c: 3, d: 4}, {e: 5, f: 6}];
 // var myObject = {c: 3, d: 4};
-// console.log(checkArrayForObject(myArray, myObject));
+// console.log(checkArrayForObject(myArray, myObject));+
 
 
 // function checkArrayForObject(array, object) {
