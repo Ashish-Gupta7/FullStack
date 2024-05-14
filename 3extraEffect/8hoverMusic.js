7d183ae9441f3c90fc6b35f08cf8d4788a4c8d63
Ashish-Gupta7 <ash300mr@gmail.com>
2024-05-15 04:41:23 +0530

    hover audio comment

--- 3extraEffect/8hoverMusic.js
+++ 3extraEffect/8hoverMusic.js
@@ -9,3 +9,5 @@ h3.addEventListener("mouseout", function() {
     audio.pause();
     audio.currentTime = 0;
 });
+
+// Modern browsers ki security policies ki wajah se, bina user interaction (jaise click, touch, etc.) ke audio play karna directly page load hone par allowed nahi hai. Yeh policy users ko unwanted aur surprise audio playback se protect karne ke liye implement ki gayi hai.
